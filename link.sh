5b99570915300a5a316c06c6a564cb247724c4d7
Bjarke Vad <bjarke.vad90@gmail.com>
2014-11-30 20:20:04 +0100

    Improved link.sh

--- link.sh
+++ link.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-ln -s `pwd`/.spacemacs ~/
-ln -s `pwd`/private ~/.emacs.d/
+
+if [ ! -f ~/.spacemacs ]; then
+    ln -s `pwd`/.spacemacs ~/
+    echo "Linked ~/.spacemacs"
+else
+    echo "~/.spacemacs already exists"
+fi
+
+if [ ! -f ~/.emacs.d/private ]; then
+    ln -s `pwd`/private ~/.emacs.d/
+    echo "Linked ~/.emacs.d/private"
+else
+    echo "~/.emacs.d/private already exists"
+fi
